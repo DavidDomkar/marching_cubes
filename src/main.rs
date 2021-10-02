4961bd4382544e7cc889b7a444d0c3b20ea3e143
David Domkář <david@domkar.dev>
2021-10-02 15:53:36 +0200

    feat: working better than before

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod marching_cubes;
 mod plugins;
 
+use std::convert::TryInto;
+use noise::Value;
+use noise::SuperSimplex;
 use noise::OpenSimplex;
 use crate::plugins::NoCameraPlayerPlugin;
 use crate::plugins::FlyCam;
@@ -9,7 +12,7 @@ use bevy::math::Vec3;
 use bevy::transform::components::Transform;
 use bevy::render2::mesh::shape::UVSphere;
 use bevy::render2::mesh::Indices;
-use noise::{NoiseFn, Perlin};
+use noise::{NoiseFn, Perlin, Seedable};
 
 use crate::marching_cubes::*;
 
@@ -50,20 +53,98 @@ fn main() {
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
 
-    let triangles = generate_mesh(100.0, 100.0, 100.0, 0.1);
+    let cell_size = 5.0;
+    let iso_level = 0.5;
+
+    let perlin = Perlin::new();
+
+    perlin.set_seed(5225);
+
+    for z in 0 .. 10 {
+        for y in 0 .. 10 {
+            for x in 0 .. 10 {
+                let translation = Vec3::new(x as f32 * cell_size - 10.0 / 2.0 * cell_size, y as f32 * cell_size - 10.0 / 2.0 * cell_size, z as f32 * cell_size - 10.0 / 2.0 * cell_size);
+
+                let value = (perlin.get([translation.x as f64 / 100.0, translation.y as f64 / 100.0, translation.z as f64 / 100.0]) * 2.0) as f32;
+
+                // println!("{:?}", translation.x as f64);
+
+                println!("{:?}", perlin.get([translation.x as f64 / 100.0, translation.y as f64 / 100.0, translation.z as f64 / 100.0]) * 2.0);
+
+                //if value < iso_level {
+                    commands.spawn_bundle(PbrBundle {
+                        mesh: meshes.add(Mesh::from(UVSphere {
+                            radius: 0.5,
+                            ..Default::default()
+                        })),
+                        material: materials.add(StandardMaterial {
+                            base_color: Color::Rgba {
+                                red: value,
+                                green: value,
+                                blue: value,
+                                alpha: 1.0,
+                            },
+                            ..Default::default()
+                        }),
+                        transform: Transform::from_translation(translation),
+                        ..Default::default()
+                    });
+                //}
+            }
+        }
+    }
+
+    let mut triangles: Vec<Triangle> = Vec::new();
 
     /*
-    let triangles = polygonise([
-        (Vec3::new(-5.0, -5.0, -5.0), 5.0),
-        (Vec3::new(5.0, -5.0, -5.0), 5.0),
-        (Vec3::new(5.0, -5.0, 5.0), 0.0),
-        (Vec3::new(-5.0, -5.0, 5.0), 5.0),
-        (Vec3::new(-5.0, 5.0, -5.0), 0.0),
-        (Vec3::new(5.0, 5.0, -5.0), 5.0),
-        (Vec3::new(5.0, 5.0, 5.0), 0.0),
-        (Vec3::new(-5.0, 5.0, 5.0), 5.0),
-    ], 3.0);
+    for z in 0 .. 10 {
+        for y in 0 .. 10 {
+            for x in 0 .. 10 {
+                let translation = Vec3::new(x as f32 * cell_size - 10.0 / 2.0 * cell_size + cell_size / 2.0, y as f32 * cell_size - 10.0 / 2.0 * cell_size + cell_size / 2.0, z as f32 * cell_size - 10.0 / 2.0 * cell_size + cell_size / 2.0);
+
+                let value = translation.distance(Vec3::ZERO) / 40.0;
+
+                let cell_points = [ translation + Vec3::new(-cell_size / 2.0, -cell_size / 2.0, -cell_size / 2.0),
+                                    translation + Vec3::new(cell_size / 2.0, -cell_size / 2.0, -cell_size / 2.0),
+                                    translation + Vec3::new(cell_size / 2.0, -cell_size / 2.0, cell_size / 2.0),
+                                    translation + Vec3::new(-cell_size / 2.0, -cell_size / 2.0, cell_size / 2.0),
+                                    translation + Vec3::new(-cell_size / 2.0, cell_size / 2.0, -cell_size / 2.0),
+                                    translation + Vec3::new(cell_size / 2.0, cell_size / 2.0, -cell_size / 2.0),
+                                    translation + Vec3::new(cell_size / 2.0, cell_size / 2.0, cell_size / 2.0),
+                                    translation + Vec3::new(-cell_size / 2.0, cell_size / 2.0, cell_size / 2.0)];
+                
+                let cell_points = [ (cell_points[0], cell_points[0].distance(Vec3::ZERO) / 40.0),
+                                    (cell_points[1], cell_points[1].distance(Vec3::ZERO) / 40.0),
+                                    (cell_points[2], cell_points[2].distance(Vec3::ZERO) / 40.0),
+                                    (cell_points[3], cell_points[3].distance(Vec3::ZERO) / 40.0),
+                                    (cell_points[4], cell_points[4].distance(Vec3::ZERO) / 40.0),
+                                    (cell_points[5], cell_points[5].distance(Vec3::ZERO) / 40.0),
+                                    (cell_points[6], cell_points[6].distance(Vec3::ZERO) / 40.0),
+                                    (cell_points[7], cell_points[7].distance(Vec3::ZERO) / 40.0)];
+
+                triangles.append(&mut polygonise(cell_points, iso_level));
+
+                if value < iso_level {
+                    commands.spawn_bundle(PbrBundle {
+                        mesh: meshes.add(Mesh::from(UVSphere {
+                            radius: 0.5,
+                            ..Default::default()
+                        })),
+                        material: materials.add(StandardMaterial {
+                            base_color: Color::RED,
+                            ..Default::default()
+                        }),
+                        transform: Transform::from_translation(translation),
+                        ..Default::default()
+                    });
+                }
+                
+            }
+        }
+    }
     */
+
+    // let triangles = generate_mesh(15.0, 15.0, 15.0, 0.1);
 
     let vertices = triangles.iter().map(|triangle| { [triangle.a, triangle.b, triangle.c] }).flatten().map(|vector| { [vector.x, vector.y, vector.z] }).collect::<Vec<_>>();
     let indices = (0..vertices.len()).map(|index| { index as u32 }).collect::<Vec<u32>>();
@@ -110,7 +191,7 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(mesh),
         material: materials.add(StandardMaterial {
-            base_color: Color::PINK,
+            base_color: Color::BLUE,
             ..Default::default()
         }),
         ..Default::default()
@@ -123,64 +204,7 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
 
     commands
         .spawn_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(-15.0, 10.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-40.0, 40.0, 40.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         }).insert(FlyCam);
-}
-
-fn generate_mesh(x: f32, y: f32, z: f32, iso_level: f32) -> Vec<Triangle> {
-    let cell_size = 5.0;
-
-    let perlin = OpenSimplex::new();
-
-    let mut triangles: Vec<Triangle> = Vec::new();
-
-    let mut current_y = -y / 2.0 + cell_size / 2.0; 
-
-    while current_y < y / 2.0 - cell_size / 2.0 {
-
-        let mut current_z = -z / 2.0 + cell_size / 2.0;
-
-        while current_z < z / 2.0 - cell_size / 2.0 {
-
-            let mut current_x = -x / 2.0 + cell_size / 2.0; 
-
-            while current_x < x / 2.0 - cell_size / 2.0 {
-
-                /*
-                triangles.append(&mut polygonise([
-                    (Vec3::new(current_x + -5.0, current_y + -5.0, current_z + -5.0), 5.0),
-                    (Vec3::new(current_x + 5.0, current_y + -5.0, current_z + -5.0), 5.0),
-                    (Vec3::new(current_x + 5.0, current_y + -5.0, current_z + 5.0), 0.0),
-                    (Vec3::new(current_x + -5.0, current_y + -5.0, current_z + 5.0), 5.0),
-                    (Vec3::new(current_x + -5.0, current_y + 5.0, current_z + -5.0), 0.0),
-                    (Vec3::new(current_x + 5.0, current_y + 5.0, current_z + -5.0), 5.0),
-                    (Vec3::new(current_x + 5.0, current_y + 5.0, current_z + 5.0), 0.0),
-                    (Vec3::new(current_x + -5.0, current_y + 5.0,current_z +  5.0), 5.0),
-                ], 3.0));
-                */
-
-                triangles.append(&mut polygonise([
-                    (Vec3::new(current_x - cell_size / 2.0, current_y - cell_size / 2.0, current_z - cell_size / 2.0), perlin.get([(current_x - cell_size / 2.0) as f64, (current_y - cell_size / 2.0) as f64, (current_z - cell_size / 2.0) as f64]) as f32),
-                    (Vec3::new(current_x + cell_size / 2.0, current_y - cell_size / 2.0, current_z - cell_size / 2.0), perlin.get([(current_x + cell_size / 2.0) as f64, (current_y - cell_size / 2.0) as f64, (current_z - cell_size / 2.0) as f64]) as f32),
-                    (Vec3::new(current_x + cell_size / 2.0, current_y - cell_size / 2.0, current_z + cell_size / 2.0), perlin.get([(current_x + cell_size / 2.0) as f64, (current_y - cell_size / 2.0) as f64, (current_z + cell_size / 2.0) as f64]) as f32),
-                    (Vec3::new(current_x - cell_size / 2.0, current_y - cell_size / 2.0, current_z + cell_size / 2.0), perlin.get([(current_x - cell_size / 2.0) as f64, (current_y - cell_size / 2.0) as f64, (current_z + cell_size / 2.0) as f64]) as f32),
-                    (Vec3::new(current_x - cell_size / 2.0, current_y + cell_size / 2.0, current_z - cell_size / 2.0), perlin.get([(current_x - cell_size / 2.0) as f64, (current_y + cell_size / 2.0) as f64, (current_z - cell_size / 2.0) as f64]) as f32),
-                    (Vec3::new(current_x + cell_size / 2.0, current_y + cell_size / 2.0, current_z - cell_size / 2.0), perlin.get([(current_x + cell_size / 2.0) as f64, (current_y + cell_size / 2.0) as f64, (current_z - cell_size / 2.0) as f64]) as f32),
-                    (Vec3::new(current_x + cell_size / 2.0, current_y + cell_size / 2.0, current_z + cell_size / 2.0), perlin.get([(current_x + cell_size / 2.0) as f64, (current_y + cell_size / 2.0) as f64, (current_z + cell_size / 2.0) as f64]) as f32),
-                    (Vec3::new(current_x - cell_size / 2.0, current_y + cell_size / 2.0, current_z + cell_size / 2.0), perlin.get([(current_x - cell_size / 2.0) as f64, (current_y + cell_size / 2.0) as f64, (current_z + cell_size / 2.0) as f64]) as f32),
-                ], iso_level));
-
-                // println!("{:?}", perlin.get([(current_x - 5.0) as f64, (current_y - 5.0) as f64, (current_z - 5.0) as f64]));
-
-                current_x += cell_size;
-            }
-
-            current_z += cell_size;
-        }
-
-        current_y += cell_size;
-    }
-
-    triangles
 }

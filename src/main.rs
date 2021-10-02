1b44a86122ebbb8e61f0be2fd80075d13200e32f
David Domkář <david@domkar.dev>
2021-10-02 04:17:48 +0200

    feat: something is working, something is broken

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod marching_cubes;
 mod plugins;
 
-use bevy::asset::AssetServer;
-use bevy::ecs::system::Res;
+use noise::OpenSimplex;
 use crate::plugins::NoCameraPlayerPlugin;
 use crate::plugins::FlyCam;
 use bevy::render2::camera::PerspectiveCameraBundle;
@@ -10,9 +9,9 @@ use bevy::math::Vec3;
 use bevy::transform::components::Transform;
 use bevy::render2::mesh::shape::UVSphere;
 use bevy::render2::mesh::Indices;
-use bevy::render2::mesh::shape::Cube;
+use noise::{NoiseFn, Perlin};
 
-use crate::marching_cubes::TRI_TABLE;
+use crate::marching_cubes::*;
 
 use bevy:: {
     PipelinedDefaultPlugins,
@@ -48,75 +47,70 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    let mut cube = Mesh::from(Cube { size: 10.0 });
 
-    let current_lookup = TRI_TABLE[1];
+    let triangles = generate_mesh(100.0, 100.0, 100.0, 0.1);
 
-    let vertices = vec![
-        [0.0, -5.0, 5.0],
-        [5.0, 0.0, 5.0],
-        [0.0, 5.0, 5.0],
-        [-5.0, 0.0, 5.0],
-        [0.0, -5.0, -5.0],
-        [5.0, 0.0, -5.0],
-        [0.0, 5.0, -5.0],
-        [-5.0, 0.0, -5.0],
-        [-5.0, -5.0, 0.0],
-        [5.0, -5.0, 0.0],
-        [5.0, 5.0, 0.0],
-        [-5.0, 5.0, 0.0],
-    ];
+    /*
+    let triangles = polygonise([
+        (Vec3::new(-5.0, -5.0, -5.0), 5.0),
+        (Vec3::new(5.0, -5.0, -5.0), 5.0),
+        (Vec3::new(5.0, -5.0, 5.0), 0.0),
+        (Vec3::new(-5.0, -5.0, 5.0), 5.0),
+        (Vec3::new(-5.0, 5.0, -5.0), 0.0),
+        (Vec3::new(5.0, 5.0, -5.0), 5.0),
+        (Vec3::new(5.0, 5.0, 5.0), 0.0),
+        (Vec3::new(-5.0, 5.0, 5.0), 5.0),
+    ], 3.0);
+    */
 
-    let mut indices: Vec<u32> = Vec::new();
+    let vertices = triangles.iter().map(|triangle| { [triangle.a, triangle.b, triangle.c] }).flatten().map(|vector| { [vector.x, vector.y, vector.z] }).collect::<Vec<_>>();
+    let indices = (0..vertices.len()).map(|index| { index as u32 }).collect::<Vec<u32>>();
+    let uvs = (0..vertices.len()).map(|_| { [0.0, 0.0] }).collect::<Vec<[f32; 2]>>();
 
-    for (_, index) in current_lookup.iter().enumerate() {
-        if *index == -1 {
-            break;
-        }
-
-        indices.push(*index as u32);
-    }
-
-    for (i, vertex) in vertices.iter().enumerate() {
+    /*
+    for vertex in vertices.iter() {
         commands.spawn_bundle(PbrBundle {
             mesh: meshes.add(Mesh::from(UVSphere {
                 radius: 0.5,
                 ..Default::default()
             })),
             material: materials.add(StandardMaterial {
-                base_color: if indices.contains(&(i as u32)) { Color::RED } else { Color::YELLOW },
+                base_color: Color::YELLOW,
                 ..Default::default()
             }),
             transform: Transform::from_translation(Vec3::from(*vertex)),
             ..Default::default()
         });
     }
+    */
 
-    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, vertices.clone());
-    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, vertices.clone());
-    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, vec![
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-        [0.0, 0.0],
-    ]);
+    
+    let mut normals: Vec<[f32; 3]> = Vec::new();
 
-    mesh.set_indices(Some(Indices::U32(indices))); 
+    for triangle in indices.chunks(3) {
+        let a = Vec3::from(vertices[(triangle)[0] as usize]);
+        let b = Vec3::from(vertices[(triangle)[1] as usize]);
+        let c = Vec3::from(vertices[(triangle)[2] as usize]);
+
+        let normal = (b - a).cross(c - a).normalize();
+
+        normals.push(normal.into());
+        normals.push(normal.into());
+        normals.push(normal.into());
+    }
+
+    mesh.set_indices(Some(Indices::U32(indices)));
+
+    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
 
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(mesh),
         material: materials.add(StandardMaterial {
-            base_color_texture: Some(asset_server.load("triangle.png")),
+            base_color: Color::PINK,
             ..Default::default()
         }),
         ..Default::default()
@@ -132,4 +126,61 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut meshes: Res
             transform: Transform::from_xyz(-15.0, 10.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         }).insert(FlyCam);
+}
+
+fn generate_mesh(x: f32, y: f32, z: f32, iso_level: f32) -> Vec<Triangle> {
+    let cell_size = 5.0;
+
+    let perlin = OpenSimplex::new();
+
+    let mut triangles: Vec<Triangle> = Vec::new();
+
+    let mut current_y = -y / 2.0 + cell_size / 2.0; 
+
+    while current_y < y / 2.0 - cell_size / 2.0 {
+
+        let mut current_z = -z / 2.0 + cell_size / 2.0;
+
+        while current_z < z / 2.0 - cell_size / 2.0 {
+
+            let mut current_x = -x / 2.0 + cell_size / 2.0; 
+
+            while current_x < x / 2.0 - cell_size / 2.0 {
+
+                /*
+                triangles.append(&mut polygonise([
+                    (Vec3::new(current_x + -5.0, current_y + -5.0, current_z + -5.0), 5.0),
+                    (Vec3::new(current_x + 5.0, current_y + -5.0, current_z + -5.0), 5.0),
+                    (Vec3::new(current_x + 5.0, current_y + -5.0, current_z + 5.0), 0.0),
+                    (Vec3::new(current_x + -5.0, current_y + -5.0, current_z + 5.0), 5.0),
+                    (Vec3::new(current_x + -5.0, current_y + 5.0, current_z + -5.0), 0.0),
+                    (Vec3::new(current_x + 5.0, current_y + 5.0, current_z + -5.0), 5.0),
+                    (Vec3::new(current_x + 5.0, current_y + 5.0, current_z + 5.0), 0.0),
+                    (Vec3::new(current_x + -5.0, current_y + 5.0,current_z +  5.0), 5.0),
+                ], 3.0));
+                */
+
+                triangles.append(&mut polygonise([
+                    (Vec3::new(current_x - cell_size / 2.0, current_y - cell_size / 2.0, current_z - cell_size / 2.0), perlin.get([(current_x - cell_size / 2.0) as f64, (current_y - cell_size / 2.0) as f64, (current_z - cell_size / 2.0) as f64]) as f32),
+                    (Vec3::new(current_x + cell_size / 2.0, current_y - cell_size / 2.0, current_z - cell_size / 2.0), perlin.get([(current_x + cell_size / 2.0) as f64, (current_y - cell_size / 2.0) as f64, (current_z - cell_size / 2.0) as f64]) as f32),
+                    (Vec3::new(current_x + cell_size / 2.0, current_y - cell_size / 2.0, current_z + cell_size / 2.0), perlin.get([(current_x + cell_size / 2.0) as f64, (current_y - cell_size / 2.0) as f64, (current_z + cell_size / 2.0) as f64]) as f32),
+                    (Vec3::new(current_x - cell_size / 2.0, current_y - cell_size / 2.0, current_z + cell_size / 2.0), perlin.get([(current_x - cell_size / 2.0) as f64, (current_y - cell_size / 2.0) as f64, (current_z + cell_size / 2.0) as f64]) as f32),
+                    (Vec3::new(current_x - cell_size / 2.0, current_y + cell_size / 2.0, current_z - cell_size / 2.0), perlin.get([(current_x - cell_size / 2.0) as f64, (current_y + cell_size / 2.0) as f64, (current_z - cell_size / 2.0) as f64]) as f32),
+                    (Vec3::new(current_x + cell_size / 2.0, current_y + cell_size / 2.0, current_z - cell_size / 2.0), perlin.get([(current_x + cell_size / 2.0) as f64, (current_y + cell_size / 2.0) as f64, (current_z - cell_size / 2.0) as f64]) as f32),
+                    (Vec3::new(current_x + cell_size / 2.0, current_y + cell_size / 2.0, current_z + cell_size / 2.0), perlin.get([(current_x + cell_size / 2.0) as f64, (current_y + cell_size / 2.0) as f64, (current_z + cell_size / 2.0) as f64]) as f32),
+                    (Vec3::new(current_x - cell_size / 2.0, current_y + cell_size / 2.0, current_z + cell_size / 2.0), perlin.get([(current_x - cell_size / 2.0) as f64, (current_y + cell_size / 2.0) as f64, (current_z + cell_size / 2.0) as f64]) as f32),
+                ], iso_level));
+
+                // println!("{:?}", perlin.get([(current_x - 5.0) as f64, (current_y - 5.0) as f64, (current_z - 5.0) as f64]));
+
+                current_x += cell_size;
+            }
+
+            current_z += cell_size;
+        }
+
+        current_y += cell_size;
+    }
+
+    triangles
 }
--- src/marching_cubes/mod.rs
+++ src/marching_cubes/mod.rs
@@ -1,3 +1,40 @@
+use bevy::math::Vec3;
+
+pub const EDGE_TABLE: [u16; 256] = [
+    0x0  , 0x109, 0x203, 0x30a, 0x406, 0x50f, 0x605, 0x70c,
+    0x80c, 0x905, 0xa0f, 0xb06, 0xc0a, 0xd03, 0xe09, 0xf00,
+    0x190, 0x99 , 0x393, 0x29a, 0x596, 0x49f, 0x795, 0x69c,
+    0x99c, 0x895, 0xb9f, 0xa96, 0xd9a, 0xc93, 0xf99, 0xe90,
+    0x230, 0x339, 0x33 , 0x13a, 0x636, 0x73f, 0x435, 0x53c,
+    0xa3c, 0xb35, 0x83f, 0x936, 0xe3a, 0xf33, 0xc39, 0xd30,
+    0x3a0, 0x2a9, 0x1a3, 0xaa , 0x7a6, 0x6af, 0x5a5, 0x4ac,
+    0xbac, 0xaa5, 0x9af, 0x8a6, 0xfaa, 0xea3, 0xda9, 0xca0,
+    0x460, 0x569, 0x663, 0x76a, 0x66 , 0x16f, 0x265, 0x36c,
+    0xc6c, 0xd65, 0xe6f, 0xf66, 0x86a, 0x963, 0xa69, 0xb60,
+    0x5f0, 0x4f9, 0x7f3, 0x6fa, 0x1f6, 0xff , 0x3f5, 0x2fc,
+    0xdfc, 0xcf5, 0xfff, 0xef6, 0x9fa, 0x8f3, 0xbf9, 0xaf0,
+    0x650, 0x759, 0x453, 0x55a, 0x256, 0x35f, 0x55 , 0x15c,
+    0xe5c, 0xf55, 0xc5f, 0xd56, 0xa5a, 0xb53, 0x859, 0x950,
+    0x7c0, 0x6c9, 0x5c3, 0x4ca, 0x3c6, 0x2cf, 0x1c5, 0xcc ,
+    0xfcc, 0xec5, 0xdcf, 0xcc6, 0xbca, 0xac3, 0x9c9, 0x8c0,
+    0x8c0, 0x9c9, 0xac3, 0xbca, 0xcc6, 0xdcf, 0xec5, 0xfcc,
+    0xcc , 0x1c5, 0x2cf, 0x3c6, 0x4ca, 0x5c3, 0x6c9, 0x7c0,
+    0x950, 0x859, 0xb53, 0xa5a, 0xd56, 0xc5f, 0xf55, 0xe5c,
+    0x15c, 0x55 , 0x35f, 0x256, 0x55a, 0x453, 0x759, 0x650,
+    0xaf0, 0xbf9, 0x8f3, 0x9fa, 0xef6, 0xfff, 0xcf5, 0xdfc,
+    0x2fc, 0x3f5, 0xff , 0x1f6, 0x6fa, 0x7f3, 0x4f9, 0x5f0,
+    0xb60, 0xa69, 0x963, 0x86a, 0xf66, 0xe6f, 0xd65, 0xc6c,
+    0x36c, 0x265, 0x16f, 0x66 , 0x76a, 0x663, 0x569, 0x460,
+    0xca0, 0xda9, 0xea3, 0xfaa, 0x8a6, 0x9af, 0xaa5, 0xbac,
+    0x4ac, 0x5a5, 0x6af, 0x7a6, 0xaa , 0x1a3, 0x2a9, 0x3a0,
+    0xd30, 0xc39, 0xf33, 0xe3a, 0x936, 0x83f, 0xb35, 0xa3c,
+    0x53c, 0x435, 0x73f, 0x636, 0x13a, 0x33 , 0x339, 0x230,
+    0xe90, 0xf99, 0xc93, 0xd9a, 0xa96, 0xb9f, 0x895, 0x99c,
+    0x69c, 0x795, 0x49f, 0x596, 0x29a, 0x393, 0x99 , 0x190,
+    0xf00, 0xe09, 0xd03, 0xc0a, 0xb06, 0xa0f, 0x905, 0x80c,
+    0x70c, 0x605, 0x50f, 0x406, 0x30a, 0x203, 0x109, 0x0,
+];
+
 pub const TRI_TABLE: [[i8; 16]; 256] = [
     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
     [0, 8, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -256,3 +293,116 @@ pub const TRI_TABLE: [[i8; 16]; 256] = [
     [0, 3, 8, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
 ];
+
+#[derive(Debug)]
+pub struct Triangle {
+    pub a: Vec3,
+    pub b: Vec3,
+    pub c: Vec3,
+}
+
+pub fn polygonise(grid: [(Vec3, f32); 8], iso_level: f32) -> Vec<Triangle> {
+    let mut cube_index: u8 = 0;
+
+    let mut vertex_list: [Option<Vec3>; 12] = [None; 12];
+    let mut triangle_list: Vec<Triangle> = Vec::with_capacity(5);
+
+    if grid[0].1 < iso_level { cube_index |= 1 };
+    if grid[1].1 < iso_level { cube_index |= 2 };
+    if grid[2].1 < iso_level { cube_index |= 4 };
+    if grid[3].1 < iso_level { cube_index |= 8 };
+    if grid[4].1 < iso_level { cube_index |= 16 };
+    if grid[5].1 < iso_level { cube_index |= 32 };
+    if grid[6].1 < iso_level { cube_index |= 64 };
+    if grid[7].1 < iso_level { cube_index |= 128 };
+
+    if EDGE_TABLE[cube_index as usize] == 0 {
+        return triangle_list;
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 1 > 0 {
+        vertex_list[0] = Some(interpolate_vertex(iso_level, &grid[0], &grid[1]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 2 > 0 {
+        vertex_list[1] = Some(interpolate_vertex(iso_level, &grid[1], &grid[2]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 4 > 0 {
+        vertex_list[2] = Some(interpolate_vertex(iso_level, &grid[2], &grid[3]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 8 > 0 {
+        vertex_list[3] = Some(interpolate_vertex(iso_level, &grid[3], &grid[0]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 16 > 0 {
+        vertex_list[4] = Some(interpolate_vertex(iso_level, &grid[4], &grid[5]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 32 > 0 {
+        vertex_list[5] = Some(interpolate_vertex(iso_level, &grid[5], &grid[6]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 64 > 0 {
+        vertex_list[6] = Some(interpolate_vertex(iso_level, &grid[6], &grid[7]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 128 > 0 {
+        vertex_list[7] = Some(interpolate_vertex(iso_level, &grid[7], &grid[4]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 256 > 0 {
+        vertex_list[8] = Some(interpolate_vertex(iso_level, &grid[0], &grid[4]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 512 > 0 {
+        vertex_list[9] = Some(interpolate_vertex(iso_level, &grid[1], &grid[5]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 1024 > 0 {
+        vertex_list[10] = Some(interpolate_vertex(iso_level, &grid[2], &grid[6]));
+    }
+
+    if EDGE_TABLE[cube_index as usize] & 2048 > 0 {
+        vertex_list[11] = Some(interpolate_vertex(iso_level, &grid[3], &grid[7]));
+    }
+
+    for i in (0..=12).step_by(3) {
+        if TRI_TABLE[cube_index as usize][i] == -1 {
+            break;
+        }
+
+        let triangle = Triangle {
+            a: vertex_list[TRI_TABLE[cube_index as usize][i] as usize].unwrap(),
+            b: vertex_list[TRI_TABLE[cube_index as usize][i + 1] as usize].unwrap(),
+            c: vertex_list[TRI_TABLE[cube_index as usize][i + 2] as usize].unwrap(),
+        };
+
+        triangle_list.push(triangle);
+    }
+
+    triangle_list
+}
+
+fn interpolate_vertex(iso_level: f32, a: &(Vec3, f32), b: &(Vec3, f32)) -> Vec3 {
+    if (iso_level - a.1).abs() < f32::EPSILON {
+        return a.0;
+    }
+
+    if (iso_level - b.1).abs() < f32::EPSILON {
+        return b.0;
+    }
+
+    if (a.1 - b.1).abs() < f32::EPSILON {
+        return a.0;
+    }
+
+    let mu = (iso_level - a.1) / (b.1 - a.1);
+
+    let x = a.0.x + mu * (b.0.x - a.0.x);
+    let y = a.0.y + mu * (b.0.y - a.0.y);
+    let z = a.0.z + mu * (b.0.z - a.0.z);
+
+    return Vec3::new(x, y, z);
+}

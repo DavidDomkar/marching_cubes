814d4429a45f8551384218ca63ed00a32f6da37a
David Domkář <david@domkar.dev>
2021-10-02 16:26:15 +0200

    feat: finally working stuff

--- src/main.rs
+++ src/main.rs
@@ -50,59 +50,28 @@ fn main() {
         .run();
 }
 
+fn value_from_noise(noise: noise::Perlin, translation: Vec3) -> f32 {
+    (noise.get([translation.x as f64 / 50.0, translation.y as f64 / 50.0, translation.z as f64 / 50.0]) * 2.0) as f32
+}
+
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
 
-    let cell_size = 5.0;
-    let iso_level = 0.5;
+    let cell_size = 2.5;
+    let iso_level = 0.7;
+
+    let number_of_cells = 200;
 
     let perlin = Perlin::new();
 
     perlin.set_seed(5225);
 
-    for z in 0 .. 10 {
-        for y in 0 .. 10 {
-            for x in 0 .. 10 {
-                let translation = Vec3::new(x as f32 * cell_size - 10.0 / 2.0 * cell_size, y as f32 * cell_size - 10.0 / 2.0 * cell_size, z as f32 * cell_size - 10.0 / 2.0 * cell_size);
-
-                let value = (perlin.get([translation.x as f64 / 100.0, translation.y as f64 / 100.0, translation.z as f64 / 100.0]) * 2.0) as f32;
-
-                // println!("{:?}", translation.x as f64);
-
-                println!("{:?}", perlin.get([translation.x as f64 / 100.0, translation.y as f64 / 100.0, translation.z as f64 / 100.0]) * 2.0);
-
-                //if value < iso_level {
-                    commands.spawn_bundle(PbrBundle {
-                        mesh: meshes.add(Mesh::from(UVSphere {
-                            radius: 0.5,
-                            ..Default::default()
-                        })),
-                        material: materials.add(StandardMaterial {
-                            base_color: Color::Rgba {
-                                red: value,
-                                green: value,
-                                blue: value,
-                                alpha: 1.0,
-                            },
-                            ..Default::default()
-                        }),
-                        transform: Transform::from_translation(translation),
-                        ..Default::default()
-                    });
-                //}
-            }
-        }
-    }
-
     let mut triangles: Vec<Triangle> = Vec::new();
 
-    /*
-    for z in 0 .. 10 {
-        for y in 0 .. 10 {
-            for x in 0 .. 10 {
-                let translation = Vec3::new(x as f32 * cell_size - 10.0 / 2.0 * cell_size + cell_size / 2.0, y as f32 * cell_size - 10.0 / 2.0 * cell_size + cell_size / 2.0, z as f32 * cell_size - 10.0 / 2.0 * cell_size + cell_size / 2.0);
-
-                let value = translation.distance(Vec3::ZERO) / 40.0;
+    for z in 0 .. number_of_cells {
+        for y in 0 .. number_of_cells {
+            for x in 0 .. number_of_cells {
+                let translation = Vec3::new(x as f32 * cell_size - number_of_cells as f32 / 2.0 * cell_size + cell_size / 2.0, y as f32 * cell_size - number_of_cells as f32 / 2.0 * cell_size + cell_size / 2.0, z as f32 * cell_size - number_of_cells as f32 / 2.0 * cell_size + cell_size / 2.0);
 
                 let cell_points = [ translation + Vec3::new(-cell_size / 2.0, -cell_size / 2.0, -cell_size / 2.0),
                                     translation + Vec3::new(cell_size / 2.0, -cell_size / 2.0, -cell_size / 2.0),
@@ -113,41 +82,24 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
                                     translation + Vec3::new(cell_size / 2.0, cell_size / 2.0, cell_size / 2.0),
                                     translation + Vec3::new(-cell_size / 2.0, cell_size / 2.0, cell_size / 2.0)];
                 
-                let cell_points = [ (cell_points[0], cell_points[0].distance(Vec3::ZERO) / 40.0),
-                                    (cell_points[1], cell_points[1].distance(Vec3::ZERO) / 40.0),
-                                    (cell_points[2], cell_points[2].distance(Vec3::ZERO) / 40.0),
-                                    (cell_points[3], cell_points[3].distance(Vec3::ZERO) / 40.0),
-                                    (cell_points[4], cell_points[4].distance(Vec3::ZERO) / 40.0),
-                                    (cell_points[5], cell_points[5].distance(Vec3::ZERO) / 40.0),
-                                    (cell_points[6], cell_points[6].distance(Vec3::ZERO) / 40.0),
-                                    (cell_points[7], cell_points[7].distance(Vec3::ZERO) / 40.0)];
+                let cell_points = [ (cell_points[0], value_from_noise(perlin, cell_points[0])),
+                                    (cell_points[1], value_from_noise(perlin, cell_points[1])),
+                                    (cell_points[2], value_from_noise(perlin, cell_points[2])),
+                                    (cell_points[3], value_from_noise(perlin, cell_points[3])),
+                                    (cell_points[4], value_from_noise(perlin, cell_points[4])),
+                                    (cell_points[5], value_from_noise(perlin, cell_points[5])),
+                                    (cell_points[6], value_from_noise(perlin, cell_points[6])),
+                                    (cell_points[7], value_from_noise(perlin, cell_points[7]))];
 
                 triangles.append(&mut polygonise(cell_points, iso_level));
-
-                if value < iso_level {
-                    commands.spawn_bundle(PbrBundle {
-                        mesh: meshes.add(Mesh::from(UVSphere {
-                            radius: 0.5,
-                            ..Default::default()
-                        })),
-                        material: materials.add(StandardMaterial {
-                            base_color: Color::RED,
-                            ..Default::default()
-                        }),
-                        transform: Transform::from_translation(translation),
-                        ..Default::default()
-                    });
-                }
-                
             }
         }
     }
-    */
 
     // let triangles = generate_mesh(15.0, 15.0, 15.0, 0.1);
 
     let vertices = triangles.iter().map(|triangle| { [triangle.a, triangle.b, triangle.c] }).flatten().map(|vector| { [vector.x, vector.y, vector.z] }).collect::<Vec<_>>();
-    let indices = (0..vertices.len()).map(|index| { index as u32 }).collect::<Vec<u32>>();
+    let mut indices = (0..vertices.len()).map(|index| { index as u32 }).collect::<Vec<u32>>();
     let uvs = (0..vertices.len()).map(|_| { [0.0, 0.0] }).collect::<Vec<[f32; 2]>>();
 
     /*
@@ -175,12 +127,14 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
         let b = Vec3::from(vertices[(triangle)[1] as usize]);
         let c = Vec3::from(vertices[(triangle)[2] as usize]);
 
-        let normal = (b - a).cross(c - a).normalize();
+        let normal = (b - a).cross(c - a).normalize() * Vec3::new(-1.0, -1.0, -1.0);
 
         normals.push(normal.into());
         normals.push(normal.into());
         normals.push(normal.into());
     }
+
+    indices.reverse();
 
     mesh.set_indices(Some(Indices::U32(indices)));
 
